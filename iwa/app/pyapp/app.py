a2a620599f910904dd10a149d4a2374c8c5cb019
shoma564 <shomaigu@gmail.com>
2022-07-12 14:18:00 +0000

    iwa

---
+++ iwa/app/pyapp/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+
+
+app = Flask(__name__, static_folder='./templates/img')
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
